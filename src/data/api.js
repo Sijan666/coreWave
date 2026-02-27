0024d8432591dc9bc663afe1e06f5e711d14b7a0
Sijan666 <666majharulislam@gmail.com>
2026-02-28 03:29:32 +0600

    add local api for reviews

---
+++ src/data/api.js
@@ -0,0 +1,31 @@
+import r1 from '../assets/r1.png'
+import r2 from '../assets/r2.png'
+import r3 from '../assets/r3.png'
+
+
+export const reviews = [
+    {
+        id : 1 ,
+        client : r1,
+        reviewTitle : "Excellent Team!",
+        desc : "The customer service team at this company was very responsive and helpful when I had questions about their products.",
+        reviewerName : "Farhan Rio",
+        sellerType : "Happy Seller",
+    },
+    {
+        id : 2 ,
+        client : r2,
+        reviewTitle : "Greate Service",
+        desc : "The customer service team at this company was very responsive and helpful when I had questions about their products.",
+        reviewerName : "Alizabeth Nancy",
+        sellerType : "Happy Seller",
+    },
+    {
+        id : 3 ,
+        client : r3,
+        reviewTitle : "The Best Agency!",
+        desc : "The customer service team at this company was very responsive and helpful when I had questions about their products.",
+        reviewerName : "Jenny Wilson",
+        sellerType : "Happy Seller",
+    },
+]
