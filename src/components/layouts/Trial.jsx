1ac3338148fa540cca2e000f0b73b040e09fb138
Sijan666 <666majharulislam@gmail.com>
2026-03-02 04:34:14 +0600

    add commenting in trial section

--- src/components/layouts/Trial.jsx
+++ src/components/layouts/Trial.jsx
@@ -10,10 +10,12 @@ const Trial = () => {
             <div className="pt-[86px] pb-[85px] bg-[#06C279] ">
                 <Container>
                     <div className="flex justify-between items-center relative">
+                        {/* left side */}
                         <div className="left">
                             <h3 className='pb-9.5 text-[45px] font-semibold text-white'>Don’t worries, start your free trial today!</h3>
                             <button className='px-[25px] py-[14.5px] rounded-[5px] bg-[#2F2F2F] text-white text-base font-semibold cursor-pointer'>Get Free Trial</button>
                         </div>
+                        {/* right side */}
                         <div className="right absolute -bottom-25 right-0">
                             <Images imgSrc={trial} className={'w-full h-[390px]'}/>
                         </div>
