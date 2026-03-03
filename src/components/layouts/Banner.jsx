105dbee381750074486b3db65f29e706a0e3293e
Sijan666 <666majharulislam@gmail.com>
2026-03-04 01:37:18 +0600

    add responsive in banner

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -8,12 +8,12 @@ const Banner = () => {
     return (
         <>
         <div className="bg-[#EEFFF9] pt-[190px]">
-            <Container>
-                <div className="flex items-center justify-between">
-                    <div className="left lg:w-[526px]">
-                        <h1 className="font-light text-[60px] text-[#1B1B1B] ">Empower Your Team <span className="font-bold">With CoreWave's</span></h1>
+            <Container className={'px-3 lg:px-0'}>
+                <div className="flex flex-col lg:flex-row items-center gap-y-10 justify-between">
+                    <div className="left lg:w-[526px] text-center lg:text-left">
+                        <h1 className="font-light text-[30px] lg:text-[60px] text-[#1B1B1B] ">Empower Your Team <span className="font-bold">With CoreWave's</span></h1>
                         <p className="pt-[37px] pb-[51px] text-[#717171] text-[20px] leading-7.5">Boost Productivity and Wellness in Your Organization with CoreWave's Advanced Tools and Techniques</p>
-                        <div className="flex gap-x-5">
+                        <div className="flex flex-col lg:flex-row gap-x-5 gap-y-5 text-center lg:text-left">
                             <div className="btn">
                                 <Button btnText={'Explore More'} className={'px-[25px] py-[14.5px]'}/>
                             </div>
