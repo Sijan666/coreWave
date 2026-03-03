d3b5535fc89607a38d655d89e062770eece0f1a2
Sijan666 <666majharulislam@gmail.com>
2026-03-04 02:17:58 +0600

    add responsive in header section

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -8,7 +8,7 @@ import { useState } from "react"
 
 const Header = () => {
 
-    const [show , setShow] = useState()
+    const [show , setShow] = useState(false)
 
     const handleClick = ()=>{
         setShow(!show)
@@ -44,29 +44,31 @@ const Header = () => {
                     </div>
                 </div>
                 {/* mobile menu */}
-                <div className="flex justify-between lg:hidden px-3 relative">
-                    <div className="logo">
-                        <Images imgSrc={Logo}/>
+                <div className="lg:hidden">
+                    <div className="flex justify-between px-3 relative">
+                        <div className="logo">
+                            <Images imgSrc={Logo}/>
+                        </div>
+                        <button onClick={handleClick}><FaBarsStaggered /></button>
                     </div>
-                    <button onClick={handleClick}><FaBarsStaggered /></button>
-                </div>
-                {show && ((
-                <div className="bg-white/5 backdrop-blur-lg rounded-[10px] py-8 w-[90%] overflow-hidden border border-gray-600 absolute top-20 left-1/2 -translate-x-1/2">
-                    <nav>
-                        <ul className="flex flex-col lg:flex-row gap-y-5 text-center gap-x-[45px]">
-                            <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Home</li>
-                            <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">About</li>
-                            <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Portfolio</li>
-                            <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Service</li>
-                            <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Blog</li>
-                            <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Testimonial</li>
-                        </ul>
-                    </nav>
-                    <div className="btn text-center pt-5">
-                        <Button btnText={'Register'} className={'px-[25px] py-[14.5px]'}/>
+                    {show && ((
+                    <div className="bg-white/5 backdrop-blur-lg rounded-[10px] py-8 w-[90%] overflow-hidden border-2 border-gray-300 absolute top-18 left-1/2 -translate-x-1/2">
+                        <nav>
+                            <ul className="flex flex-col lg:flex-row gap-y-5 text-center gap-x-[45px]">
+                                <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Home</li>
+                                <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">About</li>
+                                <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Portfolio</li>
+                                <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Service</li>
+                                <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Blog</li>
+                                <li className="text-[#1B1B1B] text-base font-medium hover:text-[#06C279] duration-300 cursor-pointer">Testimonial</li>
+                            </ul>
+                        </nav>
+                        <div className="btn text-center pt-5">
+                            <Button btnText={'Register'} className={'px-[25px] py-[14.5px]'}/>
+                        </div>
                     </div>
+                    ))}
                 </div>
-                ))}
             </Container>
         </header>
         </>
