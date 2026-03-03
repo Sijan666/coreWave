b4cc6e3baac18f315e0b62156e54748d82b4ede7
Sijan666 <666majharulislam@gmail.com>
2026-03-03 23:38:05 +0600

    add responsive in review

--- src/components/layouts/Review.jsx
+++ src/components/layouts/Review.jsx
@@ -9,9 +9,9 @@ const Review = () => {
         <>
         <section id='review'>
             <div className="py-25">
-                <Container>
-                    <h3 className='text-center text-[45px] font-semibold text-[#1B1B1B] pb-12'>What our clients saying</h3>
-                    <div className="flex justify-center gap-x-6">
+                <Container className={'px-3 lg:px-0'}>
+                    <h3 className='text-center text-[30px] md:text-[45px] font-semibold text-[#1B1B1B] pb-12'>What our clients saying</h3>
+                    <div className="flex flex-col md:flex-row md:justify-center gap-6 ">
                         {reviews.map((item)=>(
                             <div key={item.id}>
                                 <Reviewcard className={''}
